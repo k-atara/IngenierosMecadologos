c4d13f98f4928db8c288d37abcc3af158dca8c2e
aaperezon <aaron-po@hotmail.co.uk>
2022-10-21 13:22:27 -0500

    classificator

---
+++ install.sh
@@ -0,0 +1,3 @@
+pip3 install scikit-learn
+pip3 install openpyxl
+pip3 install pandas
---
+++ main.py
@@ -0,0 +1,13 @@
+from sklearn import tree
+import pandas as pd
+df = pd.read_excel('./dataset.xlsx')
+
+print(df.loc[0])
+print("================")
+print(df.loc[1])
+
+
+
+
+
+
